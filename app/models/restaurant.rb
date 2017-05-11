20e50fb8d193bfec2dd598b0cf41078a0fcc221f
anthony larzilliere <larzilliere.anthony@gmail.com>
2017-05-11 09:22:39 +0200

    ajout index.html.erb

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  # validates :phone_number, numericality: { only_integer: true }
+  validates :category, inclusion: %w(chinese italian japanese french belgian)
 end
---
+++ db/migrate/20170510132642_delete_reviews_from_restaurants.rb
@@ -0,0 +1,5 @@
+class DeleteReviewsFromRestaurants < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :restaurants, :reviews
+  end
+end
